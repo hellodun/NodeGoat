802f69e3a11dd9c7660091fd3da16618d5795ecd
Duncan G <dgatuthu@gmail.com>
2023-01-26 11:25:55 +0300

    Updated MONGO_URI string

--- config/env/all.js
+++ config/env/all.js
@@ -1,6 +1,6 @@
 // default app configuration
 const port = process.env.PORT || 4000;
-let db = process.env.MONGODB_URI || "mongodb://localhost:27017/nodegoat";
+let db = process.env.MONGODB_URI || "mongodb://nodegoat-server:NI59xvZho7pV7WVauFnmtfZPr7lecdm2yxFxLtPvLxjXrwJ6y498o4e2ttM9zFgiRUIl0LRY6KqpACDb8lOedg==@nodegoat-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@nodegoat-server";
 
 module.exports = {
     port,
